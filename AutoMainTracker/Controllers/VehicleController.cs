807d97568bde942f464a47161ab9dd3d6e345085
Chenru <Chenru@OFFICE-PC>
2017-03-10 00:14:54 -0700

    first commit

---
+++ AutoMainTracker/Controllers/VehicleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMainTracker.Models;
+
+namespace AutoMainTracker.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            var vehicle = new Vehicle() { Make = "Honda", Model = "Accord", Year = 1999, Odometer = 171000 };
+
+            return View(vehicle);
+        }
+
+        // GET: Gas
+        public ActionResult GetGas(int id)
+        {
+            //var gas = new Gas() {Make="Honda", Model="Accord", Year=1999, Odometer=171000 };
+            //return View(gas);
+            return Content("id=" + id);
+        }
+
+        // GET: Electric
+        public ActionResult GetElectric()
+        {
+            var electric = new Electric() { };
+            return View();
+        }
+
+        // GET: Diesel
+        public ActionResult GetDiesel()
+        {
+            var diesel = new Diesel() { };
+            return View();
+        }
+    }
+}
---
+++ AutoMainTracker/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoMainTracker.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+    }
+
+    public class Gas : Vehicle { }
+
+    public class Electric : Vehicle { }
+
+    public class Diesel : Vehicle { }
+}
